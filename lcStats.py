9078c1e421cf289a147d7d07ee5223fdbfff1fc8
Tim <taxelrod@gmail.com>
2018-05-13 10:02:26 -0700

    Switched from std dev to mad; added optional matching to Dump_ff file to get ra/dec/rchunk

--- lcStats.py
+++ lcStats.py
@@ -3,18 +3,24 @@
 As input, take a F_n.m file containing photometry information in ANU format
 for field n, tile m.
 
+IT IS ASSUMED THAT ALL DATA IN THE F_n_m FILE IS FOR THE SAME TILE AND FIELD!
+
 Output a light curve statistics file including for each star in the input file: V and R  
 median mag and stdev mag.
 """
 
 import numpy as np
 from scipy.stats import pearsonr
+from statsmodels.robust import mad
 import string
 import sys
+import re
+from astropy import units as u
+from astropy.coordinates import Angle
 
 debug = False
 
-def lcStats(F_fileName, Fstat_fileName, filter=True):
+def lcStats(F_fileName, Fstat_fileName, S_fileName=None, filter=True):
 
 
     fPhot = open(F_fileName)
@@ -24,6 +30,13 @@ def lcStats(F_fileName, Fstat_fileName, filter=True):
     activeTile = 0
 
     lcDict = {}
+
+    if S_fileName is not None:
+        starData = np.loadtxt(S_fileName, delimiter=';',dtype=str)
+        sTile = starData[:,1].astype(int)
+        sSeq = starData[:,2].astype(int)
+        sRchunk = starData[:,7].astype(int)
+        raDecPat = re.compile('\(([0-9-\.]+),([0-9-\.]+)\)')
     
     while not eof:
         photLine = fPhot.readline()
@@ -61,8 +74,11 @@ def lcStats(F_fileName, Fstat_fileName, filter=True):
     if debug:
         print lcDict
 
-    fStat.write('# F T S Rmed Rsigma RmeanErr Vmed Vsigma VmeanErr WScoeff WScoeffp\n')
-    
+    if S_fileName is not None:
+        fStat.write('# F T S Rchunk RA DEC Rmed Rmad RmeanErr Vmed Vmad VmeanErr WScoeff WScoeffp\n')
+    else:
+        fStat.write('# F T S Rmed Rmad RmeanErr Vmed Vmad VmeanErr WScoeff WScoeffp\n')
+
     for seq in lcDict.keys():
         lc = lcDict[seq]
         lcr = np.array(lc[0])
@@ -70,24 +86,41 @@ def lcStats(F_fileName, Fstat_fileName, filter=True):
         lcrerr = lc[2]
         lcberr = lc[3]
         lcrMedian = np.median(lcr)
-        lcrStdev = np.std(lcr)
+        lcrStdev = mad(lcr, center=lcrMedian)
         lcrAverr = np.median(lcrerr)
         lcbMedian = np.median(lcb)
-        lcbStdev = np.std(lcb)
+        lcbStdev = mad(lcb, center=lcbMedian)
         lcbAverr = np.median(lcberr)
 
         bMinusR = lcb - lcr
         wsCoeff, wsCoeffp = pearsonr(lcb-lcbMedian, lcr-lcrMedian)
-        outputLine = '%d %d %d %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f\n' % (field, tile, seq, lcrMedian, lcrStdev, lcrAverr, lcbMedian, lcbStdev, lcbAverr, wsCoeff, wsCoeffp)
+        if S_fileName is not None:
+            idStar = np.where((sTile==tile) & (sSeq==seq))
+            if len(idStar[0])==0:
+                print 'Star fts %d %d %d not found in Star file' % (field, tile, seq)
+                raise ValueError
+            thisStar = starData[idStar,:][0][0]
+            redChunk = int(thisStar[7])
+            raHMS = Angle(thisStar[3] + ' hours')
+            decDMS = Angle(thisStar[4] + ' degrees')
+            raDeg = raHMS.degree
+            decDeg = decDMS.degree
+            outputLine = '%d %d %d %d %.5f %.5f %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f\n' % (field, tile, seq, redChunk, raDeg, decDeg, lcrMedian, lcrStdev, lcrAverr, lcbMedian, lcbStdev, lcbAverr, wsCoeff, wsCoeffp)
+        else:
+            outputLine = '%d %d %d %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f\n' % (field, tile, seq, lcrMedian, lcrStdev, lcrAverr, lcbMedian, lcbStdev, lcbAverr, wsCoeff, wsCoeffp)
+
         fStat.write(outputLine)
 
     fStat.close()
             
 if __name__ == "__main__":
     
-    if len(sys.argv) != 3:
-        sys.exit('Usage: ./lcStats.py F_field.tile outputFile')
+    if len(sys.argv) < 3:
+        sys.exit('Usage: ./lcStats.py F_field.tile outputFile [DumpStar_nn]')
     
-    lcStats(sys.argv[1], sys.argv[2])
+    if len(sys.argv)==3:
+        lcStats(sys.argv[1], sys.argv[2])
+    else:
+        lcStats(sys.argv[1], sys.argv[2], sys.argv[3])
         
         

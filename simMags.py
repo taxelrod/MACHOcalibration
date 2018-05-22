25b9c9881d01a0a807950fe52d89ac2133f9d7a9
Tim <taxelrod@gmail.com>
2018-05-22 06:35:35 -0700

    Added processing of Pickles library to simMags

--- simMags.py
+++ simMags.py
@@ -4,13 +4,21 @@ import sys
 import numpy as np
 import synphot
 from synphot import units, SourceSpectrum, Observation
-from synphot.models import BlackBodyNorm1D
+from synphot.models import BlackBodyNorm1D, Empirical1D
 from synphot.spectrum import SpectralElement
+from synphot.specio import read_ascii_spec
 import astropy.units as u
+import os
 
 def readFilter(filterName):
     bp = SpectralElement.from_file(filterName, wave_unit=u.nm)
     return bp
+
+def calcMagFromSpec(bp, specPath):
+    sp = SourceSpectrum.from_file(specPath)
+    obs = Observation(sp, bp)
+    counts = obs.countrate(1.0)
+    return -2.5*np.log10(counts.value)
 
 def calcMag(bp, temp):
     bb = SourceSpectrum(BlackBodyNorm1D, temperature=temp)
@@ -18,14 +26,12 @@ def calcMag(bp, temp):
     counts = obs.countrate(1.0)
     return -2.5*np.log10(counts.value)
 
-if __name__ == "__main__":
+def magsVsTemp(filterDir, outFileName):
 
     tMin = 3000.0
     tMax = 20000.0
     tStep = 500.0
 
-    filterDir = sys.argv[1]
-    outFileName = sys.argv[2]
     outFile = open(outFileName, 'w')
 
     bp_MachoB=readFilter(filterDir+'/MACHO_Filter_B.dat')
@@ -34,7 +40,7 @@ if __name__ == "__main__":
     bp_DECam_r=readFilter(filterDir+'/DECam_r.dat')
     bp_DECam_i=readFilter(filterDir+'/DECam_i.dat')
 
-    print ('# temp MACHO_B MACHO_R DECam_g DECam_r DECam_i',file=outFile)
+    print('# temp MACHO_B MACHO_R DECam_g DECam_r DECam_i',file=outFile)
 
     temps = np.arange(tMin, tMax, tStep)
 
@@ -49,4 +55,28 @@ if __name__ == "__main__":
 
     outFile.close()
 
-    
+
+def magsPickles(filterDir, picklesDir, outFileName):
+
+    outFile = open(outFileName, 'w')
+
+    bp_MachoB=readFilter(filterDir+'/MACHO_Filter_B.dat')
+    bp_MachoR=readFilter(filterDir+'/MACHO_Filter_R.dat')
+    bp_DECam_g=readFilter(filterDir+'/DECam_g.dat')
+    bp_DECam_r=readFilter(filterDir+'/DECam_r.dat')
+    bp_DECam_i=readFilter(filterDir+'/DECam_i.dat')
+
+    print('# spec MACHO_B MACHO_R DECam_g DECam_r DECam_i',file=outFile)
+
+    for f in os.scandir(picklesDir):
+        if f.name.endswith('.dat'):
+            print(f.path)
+            MBmag = calcMagFromSpec(bp_MachoB, f.path)
+            MRmag = calcMagFromSpec(bp_MachoR, f.path)
+            gmag = calcMagFromSpec(bp_DECam_g, f.path)
+            rmag = calcMagFromSpec(bp_DECam_r, f.path)
+            imag = calcMagFromSpec(bp_DECam_i, f.path)
+            print(f.name, MBmag, MRmag, gmag, rmag, imag, file=outFile)
+
+if __name__ == "__main__":
+    magsPickles(sys.argv[1], sys.argv[2], sys.argv[3])

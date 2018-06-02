5f75fb677c02a58c4c525a8a72713aeb2576742a
Tim <taxelrod@gmail.com>
2018-06-02 10:11:44 -0700

    Added reddening; option to diddle MACHO_B qe

--- simMags.py
+++ simMags.py
@@ -3,7 +3,7 @@
 import sys
 import numpy as np
 import synphot
-from synphot import units, SourceSpectrum, Observation
+from synphot import units, SourceSpectrum, Observation, ReddeningLaw
 from synphot.models import BlackBodyNorm1D, Empirical1D
 from synphot.spectrum import SpectralElement
 from synphot.specio import read_ascii_spec
@@ -14,8 +14,10 @@ def readFilter(filterName):
     bp = SpectralElement.from_file(filterName, wave_unit=u.nm)
     return bp
 
-def calcMagFromSpec(bp, specPath):
+def calcMagFromSpec(bp, specPath, redden=None):
     sp = SourceSpectrum.from_file(specPath)
+    if redden:
+        sp = sp*redden
     obs = Observation(sp, bp)
     counts = obs.countrate(1.0)
     return -2.5*np.log10(counts.value)
@@ -36,11 +38,11 @@ def magsVsTemp(filterDir, outFileName):
 
     bp_MachoB=readFilter(filterDir+'/MACHO_Filter_B.dat')
     bp_MachoR=readFilter(filterDir+'/MACHO_Filter_R.dat')
-    bp_DECam_g=readFilter(filterDir+'/DECam_g.dat')
-    bp_DECam_r=readFilter(filterDir+'/DECam_r.dat')
-    bp_DECam_i=readFilter(filterDir+'/DECam_i.dat')
+    bp_DECam_g=readFilter(filterDir+'/DECam_noatm_g.dat')
+    bp_DECam_r=readFilter(filterDir+'/DECam_noatm_r.dat')
+    bp_DECam_i=readFilter(filterDir+'/DECam_noatm_i.dat')
 
-    print('# temp MACHO_B MACHO_R DECam_g DECam_r DECam_i',file=outFile)
+    print('# temp MACHO_B MACHO_R DECam_g DECam_r DECam_i MACHO_BR DECam_gr DECam_ri',file=outFile)
 
     temps = np.arange(tMin, tMax, tStep)
 
@@ -50,33 +52,44 @@ def magsVsTemp(filterDir, outFileName):
         gmag = calcMag(bp_DECam_g, temp)
         rmag = calcMag(bp_DECam_r, temp)
         imag = calcMag(bp_DECam_i, temp)
-        print(temp, MBmag, MRmag, gmag, rmag, imag, file=outFile)
+        print(temp, MBmag, MRmag, gmag, rmag, imag, MBmag-MRmag, gmag-rmag, rmag-imag, file=outFile)
 
 
     outFile.close()
 
 
-def magsPickles(filterDir, picklesDir, outFileName):
+def magsPickles(filterDir, picklesDir, outFileName, deltaMachoB=0):
 
+    EBV_LMC = 0.9603 # Schlafly and Finkbeiner LMC average http:..irsa.caltech.edu/applications/DUST (ridiculous!)
+    EBV_LMC = 0.2
+    extLMC = ReddeningLaw.from_extinction_model('lmcavg').extinction_curve(EBV_LMC)
+    
     outFile = open(outFileName, 'w')
 
-    bp_MachoB=readFilter(filterDir+'/MACHO_Filter_B.dat')
+    MachoBfactor = 10.0**(-0.4*deltaMachoB)
+    bp_MachoB=readFilter(filterDir+'/MACHO_Filter_B.dat') * MachoBfactor
     bp_MachoR=readFilter(filterDir+'/MACHO_Filter_R.dat')
-    bp_DECam_g=readFilter(filterDir+'/DECam_g.dat')
-    bp_DECam_r=readFilter(filterDir+'/DECam_r.dat')
-    bp_DECam_i=readFilter(filterDir+'/DECam_i.dat')
+    bp_DECam_g=readFilter(filterDir+'/DECam_noatm_g.dat')
+    bp_DECam_r=readFilter(filterDir+'/DECam_noatm_r.dat')
+    bp_DECam_i=readFilter(filterDir+'/DECam_noatm_i.dat')
+    bp_DECam_z=readFilter(filterDir+'/DECam_z.dat')
 
-    print('# spec MACHO_B MACHO_R DECam_g DECam_r DECam_i',file=outFile)
+    print('# spec MACHO_B MACHO_R DECam_g DECam_r DECam_i DECam_z MACHO_BR DECam_gr DECam_ri DECam_iz',file=outFile)
 
     for f in os.scandir(picklesDir):
         if f.name.endswith('.dat'):
             print(f.path)
-            MBmag = calcMagFromSpec(bp_MachoB, f.path)
-            MRmag = calcMagFromSpec(bp_MachoR, f.path)
-            gmag = calcMagFromSpec(bp_DECam_g, f.path)
-            rmag = calcMagFromSpec(bp_DECam_r, f.path)
-            imag = calcMagFromSpec(bp_DECam_i, f.path)
-            print(f.name, MBmag, MRmag, gmag, rmag, imag, file=outFile)
+            MBmag = calcMagFromSpec(bp_MachoB, f.path, redden=extLMC)
+            MRmag = calcMagFromSpec(bp_MachoR, f.path, redden=extLMC)
+            gmag = calcMagFromSpec(bp_DECam_g, f.path, redden=extLMC)
+            rmag = calcMagFromSpec(bp_DECam_r, f.path, redden=extLMC)
+            imag = calcMagFromSpec(bp_DECam_i, f.path, redden=extLMC)
+            zmag = calcMagFromSpec(bp_DECam_z, f.path, redden=extLMC)
+            print(f.name, MBmag, MRmag, gmag, rmag, imag, zmag, MBmag-MRmag, gmag-rmag, rmag-imag, imag-zmag, file=outFile)
 
 if __name__ == "__main__":
-    magsPickles(sys.argv[1], sys.argv[2], sys.argv[3])
+    nArg = len(sys.argv) -1
+    if nArg==3:
+        magsPickles(sys.argv[1], sys.argv[2], sys.argv[3])
+    elif nArg==4:
+        magsPickles(sys.argv[1], sys.argv[2], sys.argv[3], float(sys.argv[4]))

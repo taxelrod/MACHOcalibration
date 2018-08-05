a7ada7c238e1aadfca7f01074c7911c92ff6aeb1
Tim <taxelrod@gmail.com>
2018-08-05 11:24:26 -0700

    Fit red and blue parts of CMD separately; output FTS into synthphot output

--- fitMags.py
+++ fitMags.py
@@ -8,7 +8,8 @@ threeD = False
 matchFmt = True
 grMax = 1.4
 nIter = 5
-nSigma = 3
+nSigma = 1.5
+LMC_CMD_split = 0.1   # split in MACHO_V - MACHO_R; separate fits on each side
 
 
 if threeD:
@@ -91,7 +92,7 @@ if __name__ == "__main__":
     outFileName = sys.argv[2]
     outSynthFileName = sys.argv[3]
 
-    fOut = open(outFileName, 'a')
+    fOut = open(outFileName, 'w')
     sys.stdout = fOut
     
     print(matchFileName, file=fOut)
@@ -107,6 +108,9 @@ if __name__ == "__main__":
         MACHO_V = matchData[:,9]
         MACHO_Rerr = matchData[:,8]
         MACHO_Verr = matchData[:,11]
+        F = matchData[:,0]
+        T = matchData[:,1]
+        S = matchData[:,2]
     else:
         DECam_g = matchData[:,2]
         DECam_r = matchData[:,3]
@@ -125,46 +129,92 @@ if __name__ == "__main__":
         meas_err = meas_err[filt_gr]
         MACHO_Rerr = MACHO_Verr = DECam_gerr = DECam_rerr = DECam_ierr = meas_err        
 
-    goodId = list(range(len(DECam_g)))   #  all good to begin
-    print(-1, len(DECam_g), file=fOut)
+    print(-1, 'Total number of points:', len(DECam_g), file=fOut)
+
+# Blue side of CMD split
+
+    goodIdBlue = np.where(MACHO_V - MACHO_R < LMC_CMD_split)
+    DECam_g_Blue = DECam_g[goodIdBlue]
+    DECam_r_Blue = DECam_r[goodIdBlue]
+    DECam_gerr_Blue = DECam_gerr[goodIdBlue]
+    DECam_rerr_Blue = DECam_rerr[goodIdBlue]
+    MACHO_V_Blue = MACHO_V[goodIdBlue]
+    MACHO_R_Blue = MACHO_R[goodIdBlue]
+    MACHO_Verr_Blue = MACHO_Verr[goodIdBlue]
+    MACHO_Rerr_Blue = MACHO_Rerr[goodIdBlue]
+    F_Blue = F[goodIdBlue]
+    T_Blue = T[goodIdBlue]
+    S_Blue = S[goodIdBlue]
+    goodId = list(range(len(DECam_g_Blue))) #  all good to begin
+    
+    print(-1, 'Total number of blue points:', len(goodId), file=fOut)
     
     for n in range(nIter):
         if threeD:
             (beta, sd_beta, cov_beta, res_var) = fitODR(np.vstack((DECam_g, DECam_r, DECam_i)), np.vstack((DECam_gerr, DECam_rerr, DECam_ierr)), np.vstack((MACHO_R, MACHO_V)), np.vstack((MACHO_Rerr, MACHO_Verr)))
         else:
-            (beta, sd_beta, cov_beta, res_var) = fitODR(np.vstack((DECam_g[goodId], DECam_r[goodId])), np.vstack((DECam_gerr[goodId], DECam_rerr[goodId])), np.vstack((MACHO_R[goodId], MACHO_V[goodId])), np.vstack((MACHO_Rerr[goodId], MACHO_Verr[goodId])))
-            synth_gr = fODR(beta, np.vstack((MACHO_R, MACHO_V)))
+            (beta, sd_beta, cov_beta, res_var) = fitODR(np.vstack((DECam_g_Blue[goodId], DECam_r_Blue[goodId])), np.vstack((DECam_gerr_Blue[goodId], DECam_rerr_Blue[goodId])), np.vstack((MACHO_R_Blue[goodId], MACHO_V_Blue[goodId])), np.vstack((MACHO_Rerr_Blue[goodId], MACHO_Verr_Blue[goodId])))
+            synth_gr = fODR(beta, np.vstack((MACHO_R[goodIdBlue], MACHO_V[goodIdBlue])))
             synth_g = synth_gr[0,:]
             synth_r = synth_gr[1,:]
-            goodId_g = filterOnDist(DECam_g, synth_g, nSigma)
-            goodId_r = filterOnDist(DECam_r, synth_r, nSigma)
+            goodId_g = filterOnDist(DECam_g_Blue, synth_g, nSigma)
+            goodId_r = filterOnDist(DECam_r_Blue, synth_r, nSigma)
+            goodId = np.unique(np.concatenate((goodId_g,goodId_r)))
+            print(n, len(goodId), file=fOut)
+
+    print(np.sqrt(res_var/(3.*len(DECam_g_Blue))), file=fOut)
+    synth_g_blue = synth_g
+    synth_r_blue = synth_r
+    
+# Red side of CMD split
+
+    goodIdRed = np.where(MACHO_V - MACHO_R >= LMC_CMD_split)
+    DECam_g_Red = DECam_g[goodIdRed]
+    DECam_r_Red = DECam_r[goodIdRed]
+    DECam_gerr_Red = DECam_gerr[goodIdRed]
+    DECam_rerr_Red = DECam_rerr[goodIdRed]
+    MACHO_V_Red = MACHO_V[goodIdRed]
+    MACHO_R_Red = MACHO_R[goodIdRed]
+    MACHO_Verr_Red = MACHO_Verr[goodIdRed]
+    MACHO_Rerr_Red = MACHO_Rerr[goodIdRed]
+    F_Red = F[goodIdRed]
+    T_Red = T[goodIdRed]
+    S_Red = S[goodIdRed]
+    goodId = list(range(len(DECam_g_Red))) #  all good to begin
+
+    print(-1, 'Total number of red points:', len(goodId), file=fOut)
+    
+    for n in range(nIter):
+        if threeD:
+            (beta, sd_beta, cov_beta, res_var) = fitODR(np.vstack((DECam_g, DECam_r, DECam_i)), np.vstack((DECam_gerr, DECam_rerr, DECam_ierr)), np.vstack((MACHO_R, MACHO_V)), np.vstack((MACHO_Rerr, MACHO_Verr)))
+        else:
+            (beta, sd_beta, cov_beta, res_var) = fitODR(np.vstack((DECam_g_Red[goodId], DECam_r_Red[goodId])), np.vstack((DECam_gerr_Red[goodId], DECam_rerr_Red[goodId])), np.vstack((MACHO_R_Red[goodId], MACHO_V_Red[goodId])), np.vstack((MACHO_Rerr_Red[goodId], MACHO_Verr_Red[goodId])))
+            synth_gr = fODR(beta, np.vstack((MACHO_R[goodIdRed], MACHO_V[goodIdRed])))
+            synth_g = synth_gr[0,:]
+            synth_r = synth_gr[1,:]
+            goodId_g = filterOnDist(DECam_g_Red, synth_g, nSigma)
+            goodId_r = filterOnDist(DECam_r_Red, synth_r, nSigma)
             goodId = np.unique(np.concatenate((goodId_g,goodId_r)))
             print(n, len(goodId), file=fOut)
                    
 
-    nPts = len(synth_g)
-    outData = np.zeros((nPts, 10))
-    outData[:,0] = DECam_g
-    outData[:,1] = DECam_gerr
-    outData[:,2] = DECam_r
-    outData[:,3] = DECam_rerr
-    outData[:,4] = MACHO_R
-    outData[:,5] = MACHO_Rerr
-    outData[:,6] = MACHO_V
-    outData[:,7] = MACHO_Verr
-    outData[:,8] = synth_g.transpose()
-    outData[:,9] = synth_r.transpose()
-    np.savetxt(outSynthFileName, outData, header='DECam_g DECam_gerr DECam_r DECam_rerr MACHO_R MACHO_Rerr MACHO_B MACHO_Berr synth_g synth_r')
-    print(np.sqrt(res_var/(3.*len(DECam_g))), file=fOut)
-
-
-#        A, B = reshapeBeta(beta)
-#        printFlattened(A, fOut)
-#        printFlattened(B, fOut)
-#        A, B = reshapeBeta(sd_beta)
-#        printFlattened(A, fOut)
-#        printFlattened(B, fOut)
-#        printFlattened(cov_beta, fOut)
-#    print >>fOut, resid_g
-#    print >>fOut, resid_r
-#    print >>fOut, resid_i
+    print(np.sqrt(res_var/(3.*len(DECam_g_Red))), file=fOut)
+    synth_g_full = np.concatenate((synth_g_blue, synth_g))
+    synth_r_full = np.concatenate((synth_r_blue, synth_r))
+    
+    nPts = len(synth_g_full)
+    outData = np.zeros((nPts, 13))
+    outData[:,0] = np.concatenate((DECam_g_Blue, DECam_g_Red))
+    outData[:,1] = np.concatenate((DECam_gerr_Blue, DECam_gerr_Red))
+    outData[:,2] = np.concatenate((DECam_r_Blue, DECam_r_Red))
+    outData[:,3] = np.concatenate((DECam_rerr_Blue, DECam_rerr_Red))
+    outData[:,4] = np.concatenate((MACHO_R_Blue, MACHO_R_Red))
+    outData[:,5] = np.concatenate((MACHO_Rerr_Blue, MACHO_Rerr_Red))
+    outData[:,6] = np.concatenate((MACHO_V_Blue, MACHO_V_Red))
+    outData[:,7] = np.concatenate((MACHO_Verr_Blue, MACHO_Verr_Red))
+    outData[:,8] = synth_g_full.transpose()
+    outData[:,9] = synth_r_full.transpose()
+    outData[:,10] = np.concatenate((F_Blue, F_Red))
+    outData[:,11] = np.concatenate((T_Blue, T_Red))
+    outData[:,12] = np.concatenate((S_Blue, S_Red))
+    np.savetxt(outSynthFileName, outData, header='DECam_g DECam_gerr DECam_r DECam_rerr MACHO_R MACHO_Rerr MACHO_B MACHO_Berr synth_g synth_r F T S')  
